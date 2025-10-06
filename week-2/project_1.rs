fb5091d59f58866b2cccbf8a69ebb7ea2753d2af
MurianMarc <uchenna.nwaogu@pau.edu.ng>
2025-10-06 14:19:08 +0100

    added the class project

---
+++ week-2/project_1.rs
@@ -0,0 +1,23 @@
+fn main {
+    // defining the values
+    let p:f64 = 520_000_000;
+    let r:f64 = 10;
+    let T:f64 = 5;
+
+    
+
+    //calculate the Amount and state it
+
+    let a = p * (1 + (r/100)) * t;
+
+    println!("For an Amount of {}", a);
+
+
+
+    //calculate the Compound Interest and state it
+
+    let ci = a - p;
+
+     println!("your Compound Interest is {} naira", ci);
+
+}

3be661835a342a43dfe802ffed51e36259b9904c
MurianMarc <uchenna.nwaogu@pau.edu.ng>
2025-11-17 14:35:46 +0100

    week 7

---
+++ week_8/practice_1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let v : Vec<i64> = Vec::new();
+
+    println!("The length of Vec::new is: {}", v.len());
+
+    let v = vec!["pamler","bellingham","saka","toney","trent"];
+
+    println!("the length of vec macro is: {}",v.len());
+}
---
+++ week_8/practice_2/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+   let v = vec!['C','O','M','P','U','T','E','R'];
+
+   let mut imput1 = String::new();
+
+   println!("Enter an index value btw (0-7)");
+   std::io::stdin().read_line(&mut input1).expect("failed to read input");
+}
